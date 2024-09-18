650fe2461971d8a84aac37586621ed46eb11cfa1
SnauzA58 <123097714+SnauzA58@users.noreply.github.com>
2024-09-18 16:37:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Toggle the navigation menu on small screens
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    document.querySelector('.nav-links').classList.toggle('open');
+});
